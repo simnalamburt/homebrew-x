4f9168da4ea8ff6cf86c837eb232d6a548c6900e
Hyeon Kim <simnalamburt@gmail.com>
2023-05-04 04:34:37 +0900

    elb-log-parser.rb: bottle 0.1.1

--- Formula/elb-log-parser.rb
+++ Formula/elb-log-parser.rb
@@ -5,11 +5,11 @@ class ElbLogParser < Formula
   sha256 "4762a8a7dd8e946699358a0a8d26ffdec88996ccf509ecdcd54ff39814820940"
   license "AGPL-3.0-or-later"
 
-  # bottle do
-  #   root_url "https://github.com/simnalamburt/homebrew-x/releases/download/elb-log-parser-0.1.1"
-  #   sha256 cellar: :any_skip_relocation, arm64_ventura:  ""
-  #   sha256 cellar: :any_skip_relocation, arm64_monterey: ""
-  # end
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/elb-log-parser-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2a061150b551859009ec4dec8cd524e62e5ef9374a215f335368165c687295c4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0f9b5c78422325e63cb73fbe6a4ff8b03b04f79fb748b209dd1b8c4c41598ffd"
+  end
 
   depends_on "rust" => :build
 
