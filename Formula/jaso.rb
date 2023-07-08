61e78f4a84c1ef02b82f87c6d22937320570bd8b
Hyeon Kim <simnalamburt@gmail.com>
2023-07-09 00:03:54 +0900

    jaso 1.0.0

--- Formula/jaso.rb
+++ Formula/jaso.rb
@@ -1,12 +1,12 @@
 class Jaso < Formula
   desc "Normalizes filenames into Unicode NFC form"
   homepage "https://github.com/cr0sh/jaso"
-  url "https://github.com/cr0sh/jaso/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b253bbc0f1a17cf91fe4f3aba96a4554e28921b3e1988d1af3d8adfe65f0e18a"
+  url "https://github.com/cr0sh/jaso/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "72815436bb7a440ac53b3f79509756807e369be6082788aae40b4a00a92ad283"
 
   bottle do
-    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/jaso-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "9c223a68947f0d69d3c737f49ecb3ce1093c1de834bb9bb5812701186148953f"
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/jaso-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a473df6b23b17146133971b0277a49f5ad21cd0703eeeb9b02e503e3a844c005"
   end
 
   depends_on "rust" => :build
