2d3acbfef57cbe9be4d827135a73b046850e5576
Hyeon Kim <simnalamburt@gmail.com>
2024-10-18 14:55:04 +0900

    ra-multiplex: 0.2.5

---
+++ Formula/ra-multiplex.rb
@@ -0,0 +1,18 @@
+class RaMultiplex < Formula
+  desc "Share one rust-analyzer server instance between multiple LSP clients to save resources"
+  homepage "https://github.com/pr2502/ra-multiplex"
+  url "https://github.com/pr2502/ra-multiplex/archive/v0.2.5.tar.gz"
+  sha256 "c24a7e277adce9bbfb86641905d75f166e46459cf4e5b5f3aaa7456b052392dc"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/ra-multiplex-0.2.5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b73507838ad700a5b7641f19d0252b330a83a84ec4ee088801b727ad2e505bb6"
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
