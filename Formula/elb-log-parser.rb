b4d99e0e838ad8a324f7d16cb619a38097b732ed
Hyeon Kim <simnalamburt@gmail.com>
2024-10-21 01:37:18 +0900

    elb-log-parser: 0.2.6
    Closes #17.
    
    Signed-off-by: Jihyeon Kim (김지현) <simnalamburt@gmail.com>

--- Formula/elb-log-parser.rb
+++ Formula/elb-log-parser.rb
@@ -1,18 +1,15 @@
 class ElbLogParser < Formula
   desc "Simple AWS ELB log parser which parses Classic LB and ALB logs into JSONs"
   homepage "https://github.com/simnalamburt/elb-log-parser"
-  url "https://github.com/simnalamburt/elb-log-parser/archive/refs/tags/v0.2.3.tar.gz"
-  sha256 "0caf5b3a0d8cdaac72c4147807e34deb68ed76846a96fff962c9ea33c187c4cf"
+  url "https://github.com/simnalamburt/elb-log-parser/archive/refs/tags/v0.2.6.tar.gz"
+  sha256 "33933432ca2a0869dae5185ff5a4e3d9c3a86bd6ac798be321c2f689a2947f8b"
   license "AGPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/elb-log-parser-0.2.3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "623c545fbf9f170bdb8d5bf9a6f14d95192c95368a716b8cc4b86138c822f6ce"
-  end
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    generate_completions_from_executable(bin/"elb-log-parser", "completion")
   end
 end
