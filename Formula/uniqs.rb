7995e250e266c94a8123e223e6972087bd05d4f2
Hyeon Kim <simnalamburt@gmail.com>
2024-10-21 04:10:37 +0900

    uniqs: 0.1.1
    Closes #18.
    
    Signed-off-by: Jihyeon Kim (김지현) <simnalamburt@gmail.com>

--- Formula/uniqs.rb
+++ Formula/uniqs.rb
@@ -1,18 +1,15 @@
 class Uniqs < Formula
-  desc "`uniq(1)` alternative with streaming support"
+  desc "Rust uniq(1) alternative with streaming support"
   homepage "https://github.com/simnalamburt/uniqs"
-  url "https://github.com/simnalamburt/uniqs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "cf2e4768d3d558d8f5f26cdd28dd654b8699d9b7718ba7339f3b86ef97683d88"
+  url "https://github.com/simnalamburt/uniqs/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "0721253f5585621071c8e621ff0b30901373e19ff18f8c1220ad4f1338c1fe08"
   license "Apache-2.0" # Actually it's (Apache-2.0 OR MIT) but `brew audit` doesn't support dual licensing
-
-  bottle do
-    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/uniqs-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "01c6029435ecb21b3c33add3442a59a7d37926ee929689c8a2dcd29b3f322bc7"
-  end
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    generate_completions_from_executable(bin/"uniqs", "completion")
   end
 end
