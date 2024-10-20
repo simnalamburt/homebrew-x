25247f484e39857470d6ba52d81e238114c47207
Hyeon Kim <simnalamburt@gmail.com>
2024-10-21 04:41:26 +0900

    jaso: Misc change

--- Formula/jaso.rb
+++ Formula/jaso.rb
@@ -1,8 +1,9 @@
 class Jaso < Formula
-  desc "Normalizes filenames into Unicode NFC form"
+  desc "Normalizes filenames to Unicode NFC, faster than convmv"
   homepage "https://github.com/cr0sh/jaso"
   url "https://github.com/cr0sh/jaso/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "72815436bb7a440ac53b3f79509756807e369be6082788aae40b4a00a92ad283"
+  license "Apache-2.0" # Actually it's (Apache-2.0 OR MIT). Commenting it out due do false-alarm of "brew audit"
 
   bottle do
     root_url "https://github.com/simnalamburt/homebrew-x/releases/download/jaso-1.0.0"
