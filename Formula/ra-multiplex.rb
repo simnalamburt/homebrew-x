821dedf02dfd1e95e717327edcbd106b1cf89154
Hyeon Kim <simnalamburt@gmail.com>
2024-10-20 18:52:14 +0900

    ra-multiplex: Upload a sonoma build

--- Formula/ra-multiplex.rb
+++ Formula/ra-multiplex.rb
@@ -8,6 +8,7 @@ class RaMultiplex < Formula
   bottle do
     root_url "https://github.com/simnalamburt/homebrew-x/releases/download/ra-multiplex-0.2.5"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "b73507838ad700a5b7641f19d0252b330a83a84ec4ee088801b727ad2e505bb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "36ad6bf261a3343336cf687c8569ddfd4fe625d53c4ca490e05c3e418133bbb3"
   end
 
   depends_on "rust" => :build
