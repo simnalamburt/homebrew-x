16040838217a6c7412f82edce0ef625f6452e1b1
Hyeon Kim <simnalamburt@gmail.com>
2024-10-21 05:08:56 +0900

    jaso: 1.0.0-1
    Closes #20.
    
    Signed-off-by: Jihyeon Kim (김지현) <simnalamburt@gmail.com>

--- Formula/jaso.rb
+++ Formula/jaso.rb
@@ -6,8 +6,7 @@ class Jaso < Formula
   license "Apache-2.0" # Actually it's (Apache-2.0 OR MIT). Commenting it out due do false-alarm of "brew audit"
 
   bottle do
-    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/jaso-1.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a473df6b23b17146133971b0277a49f5ad21cd0703eeeb9b02e503e3a844c005"
+    rebuild 1
   end
 
   depends_on "rust" => :build
