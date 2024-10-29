48d301d1d4df440c12432b1155b6955405e397fe
Hyeon Kim <simnalamburt@gmail.com>
2024-10-30 03:28:48 +0900

    input-source: 0.1.4
    Closes #23.
    
    Signed-off-by: Jihyeon Kim (김지현) <simnalamburt@gmail.com>

--- Formula/input-source.rb
+++ Formula/input-source.rb
@@ -1,16 +1,9 @@
 class InputSource < Formula
   desc "Tiny input source manager for macOS"
   homepage "https://github.com/simnalamburt/macos-input-source"
-  url "https://github.com/simnalamburt/macos-input-source/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "3f72241e37539676f19d4942d757a0dcf3b37a38ddfa0f0bb49fb893bc909c94"
+  url "https://github.com/simnalamburt/macos-input-source/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "f90e522d3f6175e50f10bafd6ae44a80624029a46c68628719393f65f87bac07"
   license "Apache-2.0" # Actually it's (Apache-2.0 OR MIT). Commenting it out due do false-alarm of "brew audit"
-
-  bottle do
-    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/input-source-0.1.3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fd589ca7e1eb3b4cd6b6e6f24a0087dfd4526ec6a1bdf832dbfcb301177ddbc2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e84997970d24c2b9397adacfd97fde2179d87ee176af22014025d7ede4141ee0"
-    sha256 cellar: :any_skip_relocation, ventura:       "a83f0215f7e03fa78812398256b4a4c23bc6dea8f5a77cd264bb5dec8e0f759b"
-  end
 
   depends_on "zig" => :build
   depends_on :macos
