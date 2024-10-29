9095ceeb0e7be88c81cdf00155172af97af100b4
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2024-10-29 18:35:37 +0000

    input-source: add 0.1.4 bottle.

--- Formula/input-source.rb
+++ Formula/input-source.rb
@@ -5,6 +5,13 @@ class InputSource < Formula
   sha256 "f90e522d3f6175e50f10bafd6ae44a80624029a46c68628719393f65f87bac07"
   license "Apache-2.0" # Actually it's (Apache-2.0 OR MIT). Commenting it out due do false-alarm of "brew audit"
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/input-source-0.1.4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e41bd4726533a2c2e6011b155f463a978e902bb7d8f0adcf286b7a7d74d21c6e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0e3bc5e3742d9884d95cb58cc37d64956f053150d97f35e208aa0077fb916d9"
+    sha256 cellar: :any_skip_relocation, ventura:       "9de4c382ea183e7e529f294f5ce6a8e810363cfcd57d6c76661b688443f6c60b"
+  end
+
   depends_on "zig" => :build
   depends_on :macos
 
