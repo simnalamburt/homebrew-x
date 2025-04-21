d677c2333c94313340fe8e97d7b5a869efd96a1c
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2025-04-21 12:59:27 +0000

    ferron: add 1.0.0 bottle.

--- Formula/ferron.rb
+++ Formula/ferron.rb
@@ -6,6 +6,14 @@ class Ferron < Formula
   license "MIT"
   head "https://github.com/ferronweb/ferron.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/ferron-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9536c72c4127d8567d379e7e62623a08e71dfdc04cb2dd203d6a3be821b54be3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "563792750db9e9dd97c16d8929e9326d230e7ee6a58f79c02aa7482869240542"
+    sha256 cellar: :any_skip_relocation, ventura:       "5c0d1eb22c69bee5ab4eeb498f9266747d61e53ac9dfb65deba12082470531f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f2532316affa2b11909855520bb46cfb4113ee920fec559c1ac6574fbe0db48"
+  end
+
   depends_on "rust" => :build
 
   def install
