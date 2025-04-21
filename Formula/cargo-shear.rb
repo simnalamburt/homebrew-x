ff7995fdd9bb3c963d68d482e77641695ff6f75e
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2025-04-21 07:59:21 +0000

    cargo-shear: add 1.2.4 bottle.

--- Formula/cargo-shear.rb
+++ Formula/cargo-shear.rb
@@ -11,6 +11,14 @@ class CargoShear < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/cargo-shear-1.2.4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "809002f344643ffd9ccb90b3f70dde0a6b8644e3bc9fbc7828aed9d24d8dd63c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a3a26db96a04e958c5ce9a991507c97b951d71428919741dc718aa9a74f5f684"
+    sha256 cellar: :any_skip_relocation, ventura:       "feba090a0c2decc1e9fc7c841628bb22882f9258513106582ce81a686377a5e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e5a8c6d29e924efc3543ebf0aaacd678095114d70005e9c227b5cfc6d59ab6e"
+  end
+
   depends_on "rust" => :build
   depends_on "rustup" => :test
 
