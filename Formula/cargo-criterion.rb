88b501e78e08917964b8cba803ffe6fafc464f16
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2025-04-21 09:11:14 +0000

    cargo-criterion: add 1.1.0 bottle.

--- Formula/cargo-criterion.rb
+++ Formula/cargo-criterion.rb
@@ -13,6 +13,14 @@ class CargoCriterion < Formula
     strategy :git
   end
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/cargo-criterion-1.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05b4abdc05168d24a4e1f8a2b65f12a6c630da959a4b89fc7b1fbc70c7bdb67f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e0a178ce2953a4c733fcf777faf55eb7f275ce8425a490bffa7ab69e45f7d074"
+    sha256 cellar: :any_skip_relocation, ventura:       "30f3970ef5a08515cfbb5b4777b1314d77428eb3f9a4a552406b3edc2332946b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0183a3c7a2b5d330ffd37fad343103a24f04bbc8da7bab706a46ff3a53be792f"
+  end
+
   depends_on "rust" => :build
 
   def install
