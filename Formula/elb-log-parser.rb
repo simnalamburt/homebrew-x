df278ae2808c013b4ded3de1dddff20ac9df6d50
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2025-05-22 06:49:00 +0000

    elb-log-parser: add 0.2.7 bottle.

--- Formula/elb-log-parser.rb
+++ Formula/elb-log-parser.rb
@@ -11,6 +11,14 @@ class ElbLogParser < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/elb-log-parser-0.2.7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "53d4d45143d587f8de57e145e94aeb948fa4a340b207ae88079cb548534e4ddc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af0342a792778be931436619f37ae575c50fa4e4cee9db9fc43d44722e269d55"
+    sha256 cellar: :any_skip_relocation, ventura:       "a793fea0cb25d9009a63cba2eed10d0715aac20dfa36c1c6ecaf8475c05ecfa8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39f8a1b7904aeeadf1053759cd53a4aa6a21fecd15097cf4cd3570e0c8d7c0ca"
+  end
+
   depends_on "rust" => :build
 
   def install
