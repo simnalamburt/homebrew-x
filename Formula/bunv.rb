7997637bdfbd3082fa53f5606b05551272e5838b
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2025-07-22 09:14:53 +0000

    bunv: add 0.4.3 bottle.

--- Formula/bunv.rb
+++ Formula/bunv.rb
@@ -11,6 +11,14 @@ class Bunv < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/bunv-0.4.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ffb33654dee1edade565ab3dff5221a47d92b76bdde962176e98dc7ee7bb935d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1825503f03fde26eff14182356ee76acd6cb07bfbad038f15c4ce11387a18d3"
+    sha256 cellar: :any_skip_relocation, ventura:       "f8cd8e2dd74dbefcbaf49680bc7a6544f2272b5e0b24b61b1fa38fc38e62ff24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "960650e4e04633db4033ffd59cba7c3eb56846a1d7d33f89264422670eaedecc"
+  end
+
   depends_on "zig" => :build
 
   conflicts_with "oven-sh/bun/bun", because: "both install `bun` binaries"
