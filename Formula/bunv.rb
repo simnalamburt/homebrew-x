343e95e1915ae2bbbe7224b7d1d5a17b736871e8
Jihyeon Kim (김지현) <simnalamburt@gmail.com>
2026-02-06 06:09:17 +0000

    bunv: add 0.5.1 bottle.

--- Formula/bunv.rb
+++ Formula/bunv.rb
@@ -11,6 +11,14 @@ class Bunv < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/bunv-0.5.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d72b74a0fb230cdd39cb5c3e73c11648dadfe5c99398f79572d908699ebecc63"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59dc5aabb42f87a791eebf0141002cbf8ce6141228fc4f67f15ed69ee47c2a99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4450a7c8add2c8faf4fa8f9e1838be57186c7d91b491422e218a941e1b28e08"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "122c05140fb84ebdd06656a49805f2d1316cb8529dffc675db3fc89dc16972d8"
+  end
+
   depends_on "zig@0.15" => :build
 
   conflicts_with "oven-sh/bun/bun", because: "both install `bun` binaries"
