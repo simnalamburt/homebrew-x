4166da3f92ee75fedacdeb8c119effbeafd4a1f4
Jihyeon Kim <simnalamburt@gmail.com>
2026-02-18 15:07:00 +0900

    bunv: 0.5.2
    Closes #44.
    
    Signed-off-by: Jihyeon Kim (김지현) <simnalamburt@gmail.com>

--- Formula/bunv.rb
+++ Formula/bunv.rb
@@ -1,22 +1,14 @@
 class Bunv < Formula
   desc "Corepack for Bun. PoC for implementing version management inside bun itself"
   homepage "https://github.com/aklinker1/bunv"
-  url "https://github.com/aklinker1/bunv/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "71e148c9d2cede8812303fa9dd961a88ff2fcd32710134758dd0b4c6f3693e70"
+  url "https://github.com/aklinker1/bunv/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "a9bba1e12f85055c30fb446f36fe16d6cf75e38e6a1b787e4467c5b62aa02f83"
   license "MIT"
   head "https://github.com/aklinker1/bunv.git", branch: "main"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/simnalamburt/homebrew-x/releases/download/bunv-0.5.1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d72b74a0fb230cdd39cb5c3e73c11648dadfe5c99398f79572d908699ebecc63"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59dc5aabb42f87a791eebf0141002cbf8ce6141228fc4f67f15ed69ee47c2a99"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4450a7c8add2c8faf4fa8f9e1838be57186c7d91b491422e218a941e1b28e08"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "122c05140fb84ebdd06656a49805f2d1316cb8529dffc675db3fc89dc16972d8"
   end
 
   depends_on "zig@0.15" => :build
